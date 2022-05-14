d95f3907eb035afb11e6491e4ca645d550dd8546
Nagendra Manchuri <nagendra.manchuri@sap.com>
2022-05-14 14:59:04 +0530

    More Graph Problems

--- lc/src/main/java/org/nm/lc/medium/ShortestBridge934.java
+++ lc/src/main/java/org/nm/lc/medium/ShortestBridge934.java
@@ -1,103 +1,78 @@
 package org.nm.lc.medium;
 
-import org.apache.commons.lang3.ArrayUtils;
-import org.nm.lc.ds.StringUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class ShortestBridge934
 {
     private static final Logger logger = LoggerFactory.getLogger(ShortestBridge934.class);
 
-    int[][] dir = new int[][] { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } };
-    int mind = Integer.MAX_VALUE;
+    private int[][] dir = new int[][]{{1,0},{-1,0}, {0,1}, {0,-1}};
 
-    public int shortestBridge (int[][] grid)
-    {
-        int iscnt = 0;
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                if (grid[i][j] == 1) {
-                    dfs(i, j, grid, iscnt);
-                    iscnt++;
+    public int shortestBridge(int[][] grid) {
+        Queue<int[]> queue = new LinkedList<>();
+        boolean found = false;
+        boolean[][] visited = new boolean[grid.length][grid[0].length];
+        for(int i =0; i< grid.length;i++){
+            for(int j =0 ; j < grid[0].length; j++){
+                if(grid[i][j] == 1){
+                    dfs(i,j, grid, queue, visited);
+                    found = true;
+                }
+                if(found){
+                    break;
+                }
+            }
+            if(found){
+                break;
+            }
+        }
+        int lvl = 0;
+
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            for(int i =0 ;i < size; i++){
+                int[] cell = queue.poll();
+                int r = cell[0];
+                int c = cell[1];
+                //logger.info("{} visited cell - {}, {}.", StringUtil.getHyphens(lvl), r, c);
+
+                for(int k = 0; k < dir.length; k++){
+                    int nr = r + dir[k][0];
+                    int nc = c + dir[k][1];
+                    if(isValid(nr,nc,grid) && !visited[nr][nc]){
+                        visited[nr][nc] = true;
+                        if(grid[nr][nc] == 1){
+                            return lvl;
+                        }
+                        else{
+                            queue.add(new int[]{nr,nc});
+                        }
+                    }
+                }
+            }
+            lvl++;
+        }
+        return lvl - 1;
+    }
+
+    private void dfs(int i, int j, int[][] grid, Queue<int[]> queue, boolean[][] visited){
+        if(isValid(i,j, grid)){
+            if(grid[i][j] == 1){
+                grid[i][j] = 2;
+                visited[i][j] = true;
+                queue.add(new int[]{i,j});
+                for(int k = 0; k < dir.length; k++){
+                    dfs(i + dir[k][0], j + dir[k][1], grid, queue, visited);
                 }
             }
         }
-        mind = Math.max(grid.length, grid[0].length);
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                if (grid[i][j] == 3) {
-                    int cdist = findMin(i, j, grid, 0);
-                    mind = Math.min(cdist, mind);
-                }
-            }
-        }
-        return mind;
     }
 
-    private boolean dfs (int i, int j, int[][] grid, int cnt)
-    {
-
-        if (isValid2(i, j, grid) && grid[i][j] == 0) {
-            return true;
-        }
-        if (isValid(i, j, grid)) {
-            int val = grid[i][j];
-            boolean isBorder = false;
-            grid[i][j] = -1;
-
-            for (int k = 0; k < 4; k++) {
-                int ni = i + dir[k][0];
-                int nj = j + dir[k][1];
-                boolean borderResp = dfs(ni, nj, grid, cnt);
-                isBorder = !isBorder && borderResp ? true : isBorder;
-            }
-
-            if (isBorder) {
-                grid[i][j] = cnt == 0 ? 3 : 4;
-            }
-            else {
-                grid[i][j] = cnt == 0 ? 5 : 6;
-            }
-
-        }
-        return false;
-    }
-
-    private int findMin (int i, int j, int[][] grid, int dist)
-    {
-        if (isValid2(i, j, grid) && dist <= mind) {
-            if (grid[i][j] == 0 || (grid[i][j] == 3 && dist == 0)) {
-                int val = grid[i][j];
-                grid[i][j] = -1;
-                for (int k = 0; k < 4; k++) {
-                    int ni = i + dir[k][0];
-                    int nj = j + dir[k][1];
-                    int cdist = findMin(ni, nj, grid, dist + 1);
-                    mind = Math.min(cdist, mind);
-
-                }
-                logger.info(
-                    "{}, Index - [{}, {}],   mind - {}",
-                    StringUtil.getHyphens(dist), i, j, mind);
-                grid[i][j] = val;
-                return mind;
-            }
-            else if (grid[i][j] == 4) {
-                return dist - 1;
-            }
-        }
-        return Math.max(grid.length, grid[0].length);
-    }
-
-    private boolean isValid (int i, int j, int[][] grid)
-    {
-        return i >= 0 && j >= 0 && i < grid.length && j < grid[0].length
-            && grid[i][j] == 1;
-    }
-
-    private boolean isValid2 (int i, int j, int[][] grid)
-    {
-        return i >= 0 && j >= 0 && i < grid.length && j < grid[0].length;
+    private boolean isValid(int i, int j, int[][] grid){
+        return i>= 0 && j >= 0 && i < grid.length && j < grid[0].length;
     }
 }
